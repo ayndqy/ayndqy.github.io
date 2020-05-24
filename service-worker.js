088a386221cfca3c2adccf9fa9c6e9a6287d2964
ayndqy <59361040+ayndqy@users.noreply.github.com>
2020-05-25 02:41:20 +0300

    test

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,27 @@
-var CACHE_NAME = 'notecards-cache-v1';
+var CACHE_NAME = 'notecards-cache-v1'
 
-self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches.open(CACHE_NAME)
-		.then(function(cache) {
-			console.log('Opened cache');
-			return cache.addAll([
-				'/',
-				'./index.html',
-				'./app.css',
-				'./fonts/fonts.css',
-				'./build/bundle.js',
-				'./build/bundle.css'
-			]);
-		})
-	);
-});
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('Opened cache')
+      return cache.addAll([
+        '/',
+        './index.html',
+        './app.css',
+        './fonts/fonts.css',
+        './build/bundle.js',
+        './build/bundle.css',
+      ])
+    }),
+  )
+})
 
-self.addEventListener('fetch', function(event) {
-	console.log(event.request.url);
+self.addEventListener('fetch', function (event) {
+  console.log(event.request.url)
 
-	event.respondWith(
-		caches.match(event.request).then(function(response) {
-			return response || fetch(event.request);
-		})
-	);
-});
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request)
+    }),
+  )
+})
